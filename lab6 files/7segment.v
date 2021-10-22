b68d12ddefe55fb58258fa00721074d674c126f9
Alissa Teigland <alissa.teigland@go.winona.edu>
2021-10-22 12:06:25 -0500

    Upload
    upload constraints
    remove big comment

--- lab6 files/7segment.v
+++ lab6 files/7segment.v
@@ -1,24 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10/15/2021 01:24:04 PM
-// Design Name: 
-// Module Name: 7segment
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module seven_segment(
     input [3:0] in, // a 4 bit number 
